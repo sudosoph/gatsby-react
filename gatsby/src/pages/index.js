5fef893fd7653fd490a32480c45a003bfaa2d898
Sophia <sophia@dyn-160-39-216-133.dyn.columbia.edu>
2020-11-25 20:34:28 -0500

    Added typography and custom font

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function HomePage() {
   return (
     <>
+      <h1>Test Header</h1>
       <p>Hey - I'm the homepage.</p>
       <p>Hey I'm another element</p>
+      <hr />
+      <button type="button">Hello</button>
     </>
   );
 }
