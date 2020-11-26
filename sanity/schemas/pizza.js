fa2881c0e58a05bfb2f0aaa7901569a8992e6d67
Sophia <sophia@dyn-160-39-216-133.dyn.columbia.edu>
2020-11-26 00:00:20 -0500

    Set up pizza schema and imported to schema.js

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,41 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza', // name on computer
+  title: 'Pizzas', // public-facing name
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000),
+      // TODO: Add custom input component
+    },
+  ],
+};
